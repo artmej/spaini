87da4987154f04ee3946157d72a54aae2c6bb614
Arturo Mejia <artmej@microsoft.com>
2023-03-18 19:22:19 -0600

    add api

--- src/views/ContactoView.vue
+++ src/views/ContactoView.vue
@@ -1,5 +1,21 @@
 <template>
     <div class="contacto">
       <h1>Gracias por ponerse en contacto con nosotros.</h1>
+      {{ message }}
     </div>
   </template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: ""
+    };
+  },
+  async mounted() {
+    const { text } = await (await fetch("/api/SPAAPI")).json();
+    this.message = text;
+  }
+};
+</script>
