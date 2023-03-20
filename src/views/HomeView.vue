f62c597b6bf6975574ee49eb20012a743058a6b5
Arturo Mejia <artmej@microsoft.com>
2023-03-20 15:03:07 -0600

    auth

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <a href="/.auth/login/github">Login</a>
+    <a href="/.auth/login/github?post_login_redirect_uri=https://zealous-water.azurestaticapps.net/success">Login</a>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
