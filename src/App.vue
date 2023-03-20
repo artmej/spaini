1834565ade3db7de78ff16ab5e1a2f6dce75bfe6
Arturo Mejia <artmej@microsoft.com>
2023-03-20 15:29:19 -0600

    mav

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/contacto">Contacto</router-link>
+    <router-link to="/contacto">Contacto</router-link>|
+    <a href="/.auth/login/github?post_login_redirect_uri=https://zealous-water.azurestaticapps.net/success">Login</a>
   </nav>
   <router-view/>
 </template>
