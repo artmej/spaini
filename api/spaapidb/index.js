c3408d7a697f0018ac2aa599d2d756920a483068
Arturo Mejia <artmej@microsoft.com>
2023-03-21 22:22:19 -0600

    fix 6

--- api/spaapidb/index.js
+++ api/spaapidb/index.js
@@ -1,28 +1,20 @@
 module.exports = async function (context, req) {
-    const id = '1';
-
-    const gql = `
-      query getById($id: ID!) {
-        item_by_pk(id: $id) {
+    const query = `
+    {
+      ToDoList {
+        items {
           id
           nombre
         }
-      }`;
-  
-    const query = {
-      query: gql,
-      variables: {
-        id: id,
-      },
-    };
-  
-    const endpoint = "/data-api/graphql";
-    const response = await fetch(endpoint, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(query),
-    });
-    const result = await response.json();
+      }
+    }`;
+    
+const endpoint = "/data-api/graphql";
+const response = await fetch(endpoint, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query: query })
+});
+context.res  = await response.json();
 
-    context.res = result;
 }
