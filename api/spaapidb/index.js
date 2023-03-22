6b22774fe06a45899d3e2f0a1225d0b86642140c
Arturo Mejia <artmej@microsoft.com>
2023-03-21 19:49:34 -0600

    db config y api

---
+++ api/spaapidb/index.js
@@ -0,0 +1,28 @@
+module.exports = async function (context, req) {
+    const id = '1';
+
+    const gql = `
+      query getById($id: ID!) {
+        person_by_pk(id: $id) {
+          id
+          Name
+        }
+      }`;
+  
+    const query = {
+      query: gql,
+      variables: {
+        id: id,
+      },
+    };
+  
+    const endpoint = "/data-api/graphql";
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(query),
+    });
+    const result = await response.json();
+
+    context.res = result;
+}
